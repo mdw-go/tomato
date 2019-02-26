c946bb81ec6b7cbf65eded24e9c249c48951ede7
Michael Whatcott <mdwhatcott@gmail.com>
2019-02-26 10:20:27 -0700

    Extraced config struct and a few more functions; changed default values;

--- cmd/tomato/main.go
+++ cmd/tomato/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"log"
+	"os"
 	"os/exec"
 	"time"
 
@@ -11,21 +13,35 @@ import (
 )
 
 func main() {
-	var (
-		TomatoesPerSet          = flag.Int("tomatoes", 4, "How many tomatoes in this set?")
-		WorkPeriodInMinutes     = flag.Duration("work", time.Minute * 25, "How long is each work period?")
-		RestPeriodInMinutes     = flag.Duration("rest", time.Minute * 5, "How long is each rest period?")
-		LongRestPeriodInMinutes = flag.Duration("long-rest", time.Minute * 15, "How long is the final rest period?")
-	)
-
-	flag.Parse()
-
-	for x := 0; x < *TomatoesPerSet-1; x++ {
-		Tomato(*WorkPeriodInMinutes, *RestPeriodInMinutes)
-		fmt.Println("<ENTER> to begin the next tomato")
-		fmt.Scanln()
+	config := ReadConfiguration()
+	for range loop(config.TomatoesPerSet - 1) {
+		Tomato(config.WorkPeriod, config.RestPeriod)
+		Prompt("Press <enter> to begin next tomato...")
 	}
-	Tomato(*WorkPeriodInMinutes, *LongRestPeriodInMinutes)
+	Tomato(config.WorkPeriod, config.LongRestPeriod)
+}
+
+type Configuration struct {
+	TomatoesPerSet int
+	WorkPeriod     time.Duration
+	RestPeriod     time.Duration
+	LongRestPeriod time.Duration
+}
+
+func ReadConfiguration() (config Configuration) {
+	flag.IntVar(&config.TomatoesPerSet, "tomatoes", 4, "How many tomatoes in this set?")
+	flag.DurationVar(&config.WorkPeriod, "work", time.Minute*20, "How long is each work period?")
+	flag.DurationVar(&config.RestPeriod, "rest", time.Minute*5, "How long is each rest period?")
+	flag.DurationVar(&config.LongRestPeriod, "long-rest", time.Minute*10, "How long is the final rest period?")
+	flag.Parse()
+	return config
+}
+
+func loop(n int) []struct{} { return make([]struct{}, n) }
+
+func Prompt(message string) {
+	fmt.Print(message)
+	bufio.NewScanner(os.Stdin).Scan()
 }
 
 func Tomato(work, rest time.Duration) {
@@ -39,14 +55,22 @@ func Work(work time.Duration) {
 }
 
 func Rest(rest time.Duration) {
+	Distract()
 	Notify("Time for a " + rest.String() + " break")
 	tomato.SetTimer(rest).Start()
 }
 
 func Notify(message string) {
 	fmt.Println(message)
-	notification := fmt.Sprintf("display notification \"%s\" with title \"Tomato Timer\"", message)
-	Execute(exec.Command("osascript", "-e", notification))
+	AppleScript(fmt.Sprintf("display notification \"%s\" with title \"Tomato Timer\"", message))
+}
+
+func Distract() {
+	AppleScript(`osascript -e "tell application \"Mission Control\" to activate"`)
+}
+
+func AppleScript(script string) {
+	Execute(exec.Command("osascript", "-e", script))
 }
 
 func Execute(command *exec.Cmd) {
