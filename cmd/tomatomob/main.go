4e4afa52faa504d90558500566309ad9c5a3ca6f
Michael Whatcott <mdwhatcott@gmail.com>
2019-03-15 17:01:58 -0600

    Wrote a tomato timer for mobbing (give each driver equal typing time).

--- cmd/tomatomob/main.go
+++ cmd/tomatomob/main.go
@@ -18,17 +18,21 @@ type Configuration struct {
 	Silent         bool
 	TomatoesPerSet int
 	WorkPeriod     time.Duration
+	TeamSize       int
 }
 
 func ReadConfiguration() (config Configuration) {
 	flag.BoolVar(&config.Silent, "silent", false, "When set, refrain from audio announcements.")
 	flag.IntVar(&config.TomatoesPerSet, "tomatoes", 4, "How many tomatoes in this set?")
+	flag.IntVar(&config.TeamSize, "team", 3, "How many members in the team?")
 	flag.DurationVar(&config.WorkPeriod, "work", time.Minute*20, "How long is each work period?")
 	flag.Parse()
 	return config
 }
 
-type Session struct{ Configuration }
+type Session struct {
+	Configuration
+}
 
 func NewSession(configuration Configuration) *Session {
 	return &Session{Configuration: configuration}
@@ -36,22 +40,32 @@ func NewSession(configuration Configuration) *Session {
 
 func (this *Session) Run() {
 	for x := range loop(this.TomatoesPerSet) {
-		this.DoTomato(x == 0)
+		this.DoTomato(x == 0, x == this.TomatoesPerSet-1)
 	}
-	external.Announce(FinishedTomato, this.Silent)
 }
 
-func (this *Session) DoTomato(first bool) {
+func (this *Session) DoTomato(first bool, last bool) {
 	if !first {
 		external.Prompt(StartWhenReady)
 	}
-	Work(this.WorkPeriod, this.Silent)
-	Rest(this.Silent)
+
+	this.Work()
+
+	if !last {
+		Rest(this.Silent)
+	} else {
+		external.Announce(FinishedTomato, this.Silent)
+	}
 }
 
-func Work(work time.Duration, silent bool) {
-	external.Announce(StartingTomato, silent)
-	tomato.SetTimer(work).Start()
+func (this *Session) Work() {
+	external.Announce(StartingTomato, this.Silent)
+	for x := range loop(this.TeamSize) {
+		tomato.SetTimer(this.WorkPeriod / time.Duration(this.TeamSize)).Start()
+		if x < this.TeamSize-1 {
+			external.Announce(SwitchDriver, this.Silent)
+		}
+	}
 }
 
 func Rest(silent bool) {
@@ -63,6 +77,7 @@ const (
 	StartingTomato = "Starting tomato."
 	StoppingTomato = "Tomato concluded; time for a break."
 	FinishedTomato = "Tomato sessions complete; time for an extended break."
+	SwitchDriver   = "Switch driver"
 	StartWhenReady = "Press <enter> to ready to begin the next tomato..."
 )
 
---
+++ external/external.go
@@ -0,0 +1,39 @@
+package external
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+func Announce(message string, silent bool) {
+	fmt.Println(message)
+	if silent {
+		Notify(message)
+	} else {
+		Execute("say", "-v", "Samantha", message)
+	}
+}
+
+func Notify(message string) {
+	AppleScript(fmt.Sprintf("display notification \"%s\" with title \"Tomato Timer\"", message))
+}
+
+func MissionControl() {
+	AppleScript("tell application \"Mission Control\" to activate")
+}
+
+func AppleScript(script string) {
+	Execute("osascript", "-e", script)
+}
+
+func Execute(command string, args ...string) {
+	_ = exec.Command(command, args...).Start()
+}
+
+func Prompt(message string) {
+	fmt.Print(message)
+	bufio.NewScanner(os.Stdin).Scan()
+}
+
